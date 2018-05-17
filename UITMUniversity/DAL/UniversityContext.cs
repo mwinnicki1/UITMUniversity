a9625126165a27fba5a2fcf0bc14496c26148c46
mwinnicki1 <miloszwinnicki@gmail.com>
2018-05-18 00:05:15 +0200

    migracje, rozbudowa bazy danych

--- UITMUniversity/DAL/UniversityContext.cs
+++ UITMUniversity/DAL/UniversityContext.cs
@@ -1,29 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using UITMUniversity.Models;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
-using UITMUniversity.Models;
 
 namespace UITMUniversity.DAL
 {
     public class UniversityContext : DbContext
     {
-
-        public UniversityContext() : base("UniversityContext")
-        {
-
-        }
-
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
-
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Course>().HasMany(c => c.Instructors).WithMany(i => i.Courses).Map(t => t.MapLeftKey("CourseID").MapRightKey("InstructorID").ToTable("CourseInstructor"));
         }
-
     }
 }
